e6d22977013e2f0465d4285dea4817d7d18fc60a
Muhammad Hanzala <103304142+Muhammad-hanzala@users.noreply.github.com>
2025-05-08 03:06:16 +0500

    Update docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -6,6 +6,6 @@ def call (String ProjectName, String ImageTag, String dockerHubUser) {
                 )]) {
                   sh "docker login -u ${dockerHubUser} -p ${dockerHubPass}"
                 }
-                sh "docker push dockerHubUser}/${ProjectName}:${ImageTag} "
+                sh "docker push $(dockerHubUser}/${ProjectName}:${ImageTag} "
 }
 
